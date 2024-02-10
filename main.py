3ec88e06ad76d878ac9688e775ec1aea4b0e21b5
James Aguilar <799564+jaguilar@users.noreply.github.com>
2024-02-10 12:14:27 -0500

    simplify pid code, change polling freq

--- main.py
+++ main.py
@@ -38,24 +38,19 @@ rleg = Motor(Port.A)
 lleg = Motor(Port.D)
 arm = Motor(Port.C)
 
+target_loop_ms = 15
 
-def derr_dt_gen(errf, window_ms: int = 25, sample_ms: int = 2):  # Callable[[], int]
+
+def errderivative(errf):  # Callable[[], int]
     """Yields the change in errf per second over the last window_ms milliseconds, with samples taken every 1000 / sample_hz milliseconds."""
-    bufsize = int(window_ms / sample_ms)
+    # We're using 250ms as the window for derr/dt
+    bufsize = int(250 / target_loop_ms)
     errring = [errf()] * bufsize
     timering = [0] * bufsize
     i = 0
     sw = StopWatch()
-    derr_dt = 0
     while True:
         t = sw.time()
-
-        prev = i - 1 if i > 0 else len(timering) - 1
-        d_last_sample = t - timering[prev]
-
-        if d_last_sample < sample_ms:
-            yield derr_dt
-            continue
 
         errring[i] = errf()
         timering[i] = t
@@ -66,62 +61,78 @@ def derr_dt_gen(errf, window_ms: int = 25, sample_ms: int = 2):  # Callable[[], 
 
         derr = errring[i] - errring[windowstart]
         dt = timering[i] - timering[windowstart]
+        if dt == 0:
+            yield 0
+            continue
         derr_dt = derr * 1000.0 / dt
         i = windowstart
         yield derr_dt
 
 
-def errintegral(errf, sample_ms: int = 2):
+def errintegral(errf):
     sw = StopWatch()
     sum = 0
     while True:
         t = sw.time()
-        if t < sample_ms:
-            yield sum
-        sum += errf() * t / 1000.0
         sw.reset()
+        sum += (errf() * t) / 1000.0
+        yield sum
 
 
 def pid_controller(
     errf,  # Callable[[], int]
-    pcoeff: float = 0,
-    icoeff: float = 0,
-    dcoeff: float = 0,
+    kp: float = 0,
+    ki: float = 0,
+    kd: float = 0,
+    printit: bool = False,
 ):
-    derr_dt = derr_dt_gen(errf)
+    derivative = errderivative(errf)
     integral = errintegral(errf)
-    sw = StopWatch()
+    index = 0
     while True:
         err = errf()
-        p = err * pcoeff
-        i = next(integral) * icoeff
-        d = next(derr_dt) * dcoeff
+        p = err * kp
+        i = next(integral) * ki
+        d = next(derivative) * kd
         pid = p + i + d
-        if sw.time() > 5:
-            print(p, i, d, pid)
-            sw.reset()
+        if printit:
+            print("%d,%f,%f,%f,%f,%f" % (index, p, i, d, err, pid))
+            index += 1
         yield pid
 
 
 def main_loop():
     sw = StopWatch()
-    pid = pid_controller(gyro.angle, pcoeff=20.0, icoeff=1500, dcoeff=0)
+
+    def motor_total_travel():
+        return lleg.angle() + rleg.angle()
+
+    # We want to minimize the total motor angle.
+    anglepid = pid_controller(motor_total_travel, kp=0.0025, ki=0.005, printit=True)
+
+    def difference_from_desired_angle():
+        desired_angle = next(anglepid)
+        print(desired_angle, gyro.angle())
+        return gyro.angle() + desired_angle
+
+    pid = pid_controller(
+        difference_from_desired_angle,
+        kp=15.0,
+        ki=60,
+        kd=0,
+    )
+
     while sw.time() < 2000:
         output = next(pid)
 
     ev3.speaker.beep()
 
     sw100 = StopWatch()
-    looptime = StopWatch()
+    loopwatch = StopWatch()
     nloops = 0
 
-    # TODO: jaguilar - Install a pid controller for the target angle. Error is distance from desired
-    # wheel rotation.
-
     while True:
-        loopdelay = looptime.time()
         nloops += 1
-        looptime.reset()
 
         output = int(next(pid))
         lleg.dc(output)
@@ -130,11 +141,13 @@ def main_loop():
         if abs(output) < 100:
             sw100.reset()
         elif sw100.time() >= 1000:
-            print("motor at max for 1 sec, we crashed!", loopdelay, nloops)
+            print("motor at max for 1 sec, we crashed!", nloops)
             return
         if abs(gyro.angle()) > 45:
-            print("gyro detected at >45 offset, we crashed!", loopdelay, nloops)
+            print("gyro detected at >45 offset, we crashed!", nloops)
             return
+        wait(max(0, target_loop_ms - loopwatch.time()))
+        loopwatch.reset()
 
 
 if __name__ == "__main__":
